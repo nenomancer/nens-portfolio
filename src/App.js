7eb070c075fa660e2fa2f3b546c3ad67c6028b76
Nenomancer <nebojsa.kovacevik@gmail.com>
2022-03-07 14:29:51 +0100

    Added gridSize attribute to all tiles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       src: "gallery/earf.png",
       alt: "earf",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "",
     },
     {
       id: "2",
@@ -19,6 +20,7 @@ function App() {
       src: "gallery/pluto.png",
       alt: "pluto",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "wide",
     },
     {
       id: "3",
@@ -26,6 +28,7 @@ function App() {
       src: "gallery/river.jpg",
       alt: "river",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "tall",
     },
     {
       id: "4",
@@ -33,6 +36,7 @@ function App() {
       src: "gallery/philharmony.png",
       alt: "philharmony",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "wide tall",
     },
     {
       id: "5",
@@ -40,6 +44,7 @@ function App() {
       src: "gallery/earf.png",
       alt: "earf",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "",
     },
     {
       id: "6",
@@ -47,6 +52,7 @@ function App() {
       src: "gallery/pluto.png",
       alt: "pluto",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "",
     },
     {
       id: "7",
@@ -54,6 +60,7 @@ function App() {
       src: "gallery/river.jpg",
       alt: "river",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "wide",
     },
     {
       id: "8",
@@ -61,6 +68,7 @@ function App() {
       src: "gallery/philharmony.png",
       alt: "philharmony",
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      gridSize: "tall",
     },
   ];
 
