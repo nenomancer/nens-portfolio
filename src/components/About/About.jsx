c7f60fd5eb387d7a00f28db935b779efb115db42
Neno <nebojsa.kovacevik@gmail.com>
2022-05-04 14:53:10 +0200

    Icons without links are rendered as divs; try to make Icon components, and iterate through them

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -30,7 +30,7 @@ const About = (props) => {
     },
   ];
 
-  const skillsIcons = [
+  const visualIcons = [
     {
       type: "visual",
       name: "Photoshop",
@@ -42,28 +42,52 @@ const About = (props) => {
       type: "visual",
       name: "After Effects",
       icon: "icons/software/after-effects.png",
-      info: "Additional info perhaps",
+      info: "After Effects: Advanced knowledge ",
       level: 9,
     },
     {
       type: "visual",
       name: "Premiere Pro",
       icon: "icons/software/premiere.png",
-      info: "Additional info perhaps",
+      info: "Premier Pro: Very good knowledge of most of it's capabilities",
       level: 8,
     },
     {
       type: "visual",
       name: "DaVinci",
       icon: "icons/software/davinci.png",
-      info: "Additional info perhaps",
+      info: "Davinci Resolve: Expert level knowledge of all it's capabilities",
       level: 10,
     },
     {
       type: "visual",
       name: "Blender",
       icon: "icons/software/blender.png",
-      info: "Additional info perhaps",
+      info: "Blender: Expert level knowledge of all it's capabilities",
+      level: 10,
+    },
+    {
+      type: "visual",
+      name: "Nuke",
+      icon: "icons/software/nuke.png",
+      info: "NUKE: Expert level knowledge of all it's capabilities",
+      level: 10,
+    },
+  ];
+
+  const audioIcons = [
+    {
+      type: "audio",
+      name: "ableton",
+      icon: "icons/software/ableton.png",
+      info: "Ableton: Expert level knowledge of all it's capabilities",
+      level: 10,
+    },
+    {
+      type: "audio",
+      name: "maschine",
+      icon: "icons/software/maschine.png",
+      info: "NUKE: Expert level knowledge of all it's capabilities",
       level: 10,
     },
   ];
@@ -75,10 +99,18 @@ const About = (props) => {
           <div className="about__picture">
             <img src="gallery/profile-pic.jpg" alt="" />
           </div>
-
+          <h2>Contact</h2>
           <AboutSkills title="Contact" icons={socialIcons} />
 
-          <AboutSkills title="Skills" icons={skillsIcons} />
+          <h2>Skills</h2>
+          <h3>Visual</h3>
+          <AboutSkills title="Skills" icons={visualIcons} />
+
+          <h3>Audio</h3>
+          <AboutSkills title="Skills" icons={audioIcons} />
+
+          <h3>Code</h3>
+          <AboutSkills title="Skills" icons={visualIcons} />
           {/* <div className="about__skills">
             <h2>Skills:</h2>
             <img
@@ -98,7 +130,8 @@ const About = (props) => {
         </div>
         <div className="about__right">
           <div className="about__info">
-            <h2 className="about__h2">About Me</h2>
+            {/* <h1 className="about__h2">About Me</h1> */}
+            <h1>Nebojsa Kovacevik</h1>
             <p>
               This website has been my project for the last few months. I made
               it in order to sharpen my web development skills (mainly React and
