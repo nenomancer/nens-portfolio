3720e1eb65a18d56725b879bee4794cfa08fca86
Neno <nebojsa.kovacevik@gmail.com>
2022-06-29 00:13:51 +0200

    Refactor Sidebar into component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { Fragment, useEffect, useState } from "react";
 import { ArtworkData } from "./ArtworkData";
 // import About from "./components/About/About";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/Sidebar";
 import Gallery from "./components/Gallery";
 
 // IMPORT SIDEBAR ICONS
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdAutoAwesomeMosaic } from "react-icons/md";
-import { MdPhotoLibrary } from "react-icons/md";
-import { MdVideoLibrary } from "react-icons/md";
-import { MdLibraryMusic } from "react-icons/md";
+
 import { MdInfo } from "react-icons/md";
 import { MdPerson } from "react-icons/md";
 
@@ -18,23 +15,37 @@ import { GrInstagram } from "react-icons/gr";
 import { GrLinkedin } from "react-icons/gr";
 import { GrFacebook } from "react-icons/gr";
 import About from "./components/About";
+import Aside from "./components/Sidebar";
 
 // import variables from "./index.scss";
 
 function App() {
   // GALLERY FILTER STATES
 
-  const [filterType, setFilterType] = useState("all"); // This is for the top filterType bar
+  const [galleryFilter, setGalleryFilter] = useState("all"); // This is for the top galleryFilter bar
   const [filteredTiles, setFilteredTiles] = useState([]); // This is for the ArtworkData tiles
 
   // FILTER THROUGH TILES BASED ON FILTER TYPE
+  // useEffect(() => {
+  //   galleryFilter === "all"
+  //     ? setFilteredTiles(ArtworkData)
+  //     : setFilteredTiles(
+  //         ArtworkData.filter((item) => item.type.includes(galleryFilter))
+  //       );
+  // }, [galleryFilter]);
+
   useEffect(() => {
-    filterType === "all"
-      ? setFilteredTiles(ArtworkData)
-      : setFilteredTiles(
-          ArtworkData.filter((item) => item.type.includes(filterType))
-        );
-  }, [filterType]);
+    if (galleryFilter === "all") {
+      setFilteredTiles(ArtworkData);
+    } else {
+      console.log(galleryFilter);
+      ArtworkData.map((tile) => {
+        if (tile.type.includes(galleryFilter)) {
+          tile.classList.add("visible");
+        }
+      });
+    }
+  }, [galleryFilter]);
 
   // IMAGE MODAL STATES
   const [showTileModal, setShowTileModal] = useState(false);
@@ -47,23 +58,23 @@ function App() {
   };
 
   // MAKE ARRAY OUT OF MENU ITEMS
-  const menuItems = [...document.querySelectorAll(".sidebar-list-item")];
+  // const menuItems = [...document.querySelectorAll(".sidebar-list-item")];
 
-  const menuClickHandler = (e) => {
-    // closeMenu();
-    // toggleMenu();
-    toggleAbout("close");
-    // SET FILTER TYPE BASED ON TEXT CONTENT OF MENU ITEM
-    setFilterType(e.target.textContent.toLowerCase());
-    // SET ACTIVE CLASS FOR CLICKED ITEM
-    menuItems.forEach((item) => {
-      if (item.textContent !== e.target.textContent) {
-        item.classList.remove("active");
-      } else {
-        e.target.classList.add("active");
-      }
-    });
-  };
+  // const menuClickHandler = (e) => {
+  //   // closeMenu();
+  //   // toggleMenu();
+  //   toggleAbout("close");
+  //   // SET FILTER TYPE BASED ON TEXT CONTENT OF MENU ITEM
+  //   setGalleryFilter(e.target.textContent.toLowerCase());
+  //   // SET ACTIVE CLASS FOR CLICKED ITEM
+  //   menuItems.forEach((item) => {
+  //     if (item.textContent !== e.target.textContent) {
+  //       item.classList.remove("active");
+  //     } else {
+  //       e.target.classList.add("active");
+  //     }
+  //   });
+  // };
 
   const sidebar = document.querySelector(".sidebar");
   const listItemTexts = [...document.querySelectorAll(".list-item-text")];
@@ -98,7 +109,6 @@ function App() {
   const aboutCard = document.querySelector(".about-card");
   const aboutButton = document.querySelector("#about-button");
 
-
   const toggleAbout = (state) => {
     if (state === "toggle") {
       aboutContainer.classList.toggle("open");
@@ -110,7 +120,6 @@ function App() {
       aboutButton.classList.remove("about-btn-toggle");
     }
   };
-
 
   return (
     <div className="container">
@@ -126,131 +135,20 @@ function App() {
           id="about-button"
           onClick={() => toggleAbout("toggle")}
         >
-            <MdInfo className="icon sidebar-icon" />
+          <MdInfo className="icon sidebar-icon" />
         </div>
-     
       </header>
       <main>
-        <aside className="sidebar">
-          <ul className="sidebar-list">
-            <li className="sidebar-list-item active" onClick={menuClickHandler}>
-              <div className="list-item-icon">
-                <MdAutoAwesomeMosaic className="icon sidebar-icon" />
-              </div>
-              <div className="list-item-text active">All</div>
-            </li>
-            <li className="sidebar-list-item " onClick={menuClickHandler}>
-              <div className="list-item-icon">
-                <MdPhotoLibrary className="icon sidebar-icon" />
-              </div>
-              <div className="list-item-text">Images</div>
-            </li>
-            <li className="sidebar-list-item " onClick={menuClickHandler}>
-              <div className="list-item-icon">
-                <MdVideoLibrary className="icon sidebar-icon" />
-              </div>
-              <div className="list-item-text">Videos</div>
-            </li>
-            <li className="sidebar-list-item " onClick={menuClickHandler}>
-              <div className="list-item-icon">
-                <MdLibraryMusic className="icon sidebar-icon" />
-              </div>
-              <div className="list-item-text">Audio</div>
-            </li>
-            {/* <li
-              className="sidebar-list-item"
-              id="about-list-item"
-              onClick={() => toggleAbout("toggle")}
-            >
-              <div className="list-item-icon">
-                <MdInfo className="icon sidebar-icon" />
-              </div>
-              <div className="list-item-text">About</div>
-            </li> */}
-          </ul>
-        </aside>
-          {/* <div className="about-container">
-            <div className="about-card">
-              <div className="about-img"></div>
-              <div className="about-content">
-                <div className="about-section">
-                  <div className="about-title">
-                    <h1>About Me</h1>
-                  </div>
-                  <p>
-                    Hello there! My name is Nebojsa Kovacevik and I am a
-                    passionate front-end developer, always interested in
-                    learning and implementing new technologies.
-                  </p>
-                  <p>
-                    As a senior VFX artist and CG generalist for the past 6+
-                    years, I have been tasked with problems of high complexity,
-                    which required not only artistic skills, but logical
-                    problem-solving, automation and optimization.
-                  </p>
-                  <p>
-                    Aside from my extensive experience in VFX, I've done many
-                    personal projects which require skills from other fields,
-                    varying from front-end development, to hand-drawn 2D
-                    animation.
-                  </p>
-                </div>
-
-                <div className="about-section">
-                  <div className="about-title">
-                    <h1>Services</h1>
-                    <span></span>
-                  </div>
-                  <ul>
-                    <li>Web Developoment</li>
-                    <li>3D & Graphic Design</li>
-                    <li>Animation & Motion Graphics</li>
-                  </ul>
-                  <ul>
-                    <li>Web Developoment</li>
-                    <li>3D & Graphic Design</li>
-                    <li>Animation & Motion Graphics</li>
-                  </ul>
-               
-                </div>
-
-                <div className="about-section">
-                  <div className="left">
-                    <div className="about-title">
-                      <h1>Experience</h1>
-                      <span></span>
-                    </div>
-                    <ul>
-                      <li>Web Developoment</li>
-                      <li>3D & Graphic Design</li>
-                      <li>Animation & Motion Graphics</li>
-                    </ul>
-                  </div>
-
-                  <div className="right">
-                    <div className="about-title">
-                      <h1>Interests</h1>
-                      <span></span>
-                    </div>
-                    <ul>
-                      <li>Web Developoment</li>
-                      <li>3D & Graphic Design</li>
-                      <li>Animation & Motion Graphics</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
-          <About />
-          <Gallery
-            artwork={filteredTiles}
-            viewHandler={viewHandler}
-            showTileModal={showTileModal}
-            setShowTileModal={setShowTileModal}
-            selectedImg={selectedImg}
-            setSelectedImg={setSelectedImg}
-          />
+        <Sidebar ArtworkData={ArtworkData}/>
+        {/* <About /> */}
+        <Gallery
+          artwork={ArtworkData}
+          viewHandler={viewHandler}
+          showTileModal={showTileModal}
+          setShowTileModal={setShowTileModal}
+          selectedImg={selectedImg}
+          setSelectedImg={setSelectedImg}
+        />
       </main>
     </div>
   );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "../scss/Sidebar.scss";
+
+import { MdAutoAwesomeMosaic } from "react-icons/md";
+import { MdPhotoLibrary } from "react-icons/md";
+import { MdVideoLibrary } from "react-icons/md";
+import { MdLibraryMusic } from "react-icons/md";
+
+const Sidebar = (props) => {
+  const menuItems = [...document.querySelectorAll(".sidebar-list-item")];
+
+  const menuClickHandler = (e) => {
+    if (e === "all") {
+      console.log(`EVENT IS ${e}`);
+      return;
+    } else {
+      console.log(props.galleryFilter);
+      props.ArtworkData.map((tile) => {
+        if (tile.type.includes(props.galleryFilter)) {
+          tile.classList.add("visible");
+        }
+      });
+    }
+    // closeMenu();
+    // toggleMenu();
+    // props.toggleAbout("close");
+    // SET FILTER TYPE BASED ON TEXT CONTENT OF MENU ITEM
+    // props.setGalleryFilter(e.target.textContent.toLowerCase());
+    // SET ACTIVE CLASS FOR CLICKED ITEM
+    menuItems.forEach((item) => {
+      if (item.textContent !== e.target.textContent) {
+        item.classList.remove("active");
+      } else {
+        e.target.classList.add("active");
+      }
+    });
+  };
+
+  return (
+    <aside className="sidebar">
+      <ul className="sidebar-list">
+        <li
+          className="sidebar-list-item active"
+          onClick={() => menuClickHandler("all")}
+        >
+          <div className="list-item-icon">
+            <MdAutoAwesomeMosaic className="icon sidebar-icon" />
+          </div>
+          <div className="list-item-text active">All</div>
+        </li>
+        <li className="sidebar-list-item " onClick={menuClickHandler}>
+          <div className="list-item-icon">
+            <MdPhotoLibrary className="icon sidebar-icon" />
+          </div>
+          <div className="list-item-text">Images</div>
+        </li>
+        <li className="sidebar-list-item " onClick={menuClickHandler}>
+          <div className="list-item-icon">
+            <MdVideoLibrary className="icon sidebar-icon" />
+          </div>
+          <div className="list-item-text">Videos</div>
+        </li>
+        <li className="sidebar-list-item " onClick={menuClickHandler}>
+          <div className="list-item-icon">
+            <MdLibraryMusic className="icon sidebar-icon" />
+          </div>
+          <div className="list-item-text">Audio</div>
+        </li>
+        {/* <li
+              className="sidebar-list-item"
+              id="about-list-item"
+              onClick={() => toggleAbout("toggle")}
+            >
+              <div className="list-item-icon">
+                <MdInfo className="icon sidebar-icon" />
+              </div>
+              <div className="list-item-text">About</div>
+            </li> */}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
