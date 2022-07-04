60c1121cbe00af0b5dd70ab9ce43d497f81834ab
Neno <nebojsa.kovacevik@gmail.com>
2022-07-04 13:43:46 +0200

    Begin work on animating header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdInfo } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
-import "../scss/Header.scss"
+import "../scss/Header.scss";
 
 const Header = (props) => {
   const sidebar = document.querySelector(".sidebar");
@@ -11,18 +13,30 @@ const Header = (props) => {
 
   const aboutContainer = document.querySelector(".about-container");
   const aboutCard = document.querySelector(".about-card");
-  const aboutButton = document.querySelector("#about-button");
 
-  const toggleAbout = (state) => {
-    if (state === "toggle") {
-      aboutContainer.classList.toggle("open");
-      // document.querySelector(".gallery").classList.toggle("open");
-      aboutButton.classList.toggle("about-btn-toggle");
-    } else if (state === "close") {
-      aboutContainer.classList.remove("open");
-      // document.querySelector(".gallery").classList.remove("open");
-      aboutButton.classList.remove("about-btn-toggle");
-    }
+  const [openAbout, closeAbout] = [
+    ...document.querySelectorAll(".about-button"),
+  ];
+
+  // const toggleAbout = (state) => {
+  //   if (state === "toggle") {
+  //     aboutContainer.classList.toggle("open");
+  //     openAbout.style.transform = "translateX(0)";
+  //     closeAbout.style.transform = "translateX(100%)";
+  //   } else if (state === "close") {
+  //     aboutContainer.classList.remove("open");
+  //     // document.querySelector(".gallery").classList.remove("open");
+  //     openAbout.style.transform = "translateX(100%)";
+  //     closeAbout.style.transform = "translateX(0)";
+  //   }
+  // };
+
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+
+  const toggleAbout = () => {
+    aboutContainer.classList.toggle("open");
+    openAbout.classList.toggle("active-button");
+    closeAbout.classList.toggle("active-button");
   };
 
   let isMenuOpen = false;
@@ -51,17 +65,14 @@ const Header = (props) => {
   };
   return (
     <header className="header">
-      <div className="header-button" onMouseUp={toggleMenu}>
+      <div id="toggle-menu" className="header-button" onMouseUp={toggleMenu}>
         <GiHamburgerMenu className="icon" />
       </div>
       <span className="header-name">NENS</span>
 
-      <div
-        className="header-button"
-        id="about-button"
-        onClick={() => toggleAbout("toggle")}
-      >
-        <MdInfo className="icon sidebar-icon" />
+      <div className="header-button" id="about-button" onClick={toggleAbout}>
+        <MdInfo className="icon about-button active-button" id="open-about" />
+        <IoClose className="icon about-button" id="close-about" />
       </div>
     </header>
   );
