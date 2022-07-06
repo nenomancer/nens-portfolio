0ecf9b305697c71561b5c4bb4cdcd616e9f16cc5
Neno <nebojsa.kovacevik@gmail.com>
2022-07-06 16:21:17 +0200

    Fix About stylesheet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,170 +9,7 @@ import { BsInstagram } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 
 const About = (props) => {
-  // const socialIcons = [
-  //   {
-  //     name: "Facebook",
-  //     link: "https://www.facebook.com/nebojsa.kovacevik/",
-  //     icon: "icons/social/facebook-brands.svg",
-  //     info: "My Facebook profile",
-  //   },
-  //   {
-  //     name: "Instagram",
-  //     link: "https://www.instagram.com/loopmaster.n/",
-  //     icon: "icons/social/instagram.svg",
-  //     info: "My Instagram profile",
-  //   },
-  //   {
-  //     name: "Github",
-  //     link: "https://github.com/nenomancer",
-  //     icon: "icons/social/github.svg",
-  //     info: "My Github profile",
-  //   },
-  //   {
-  //     name: "LinkedIn",
-  //     link: "https://www.linkedin.com/in/nebojsa-kovacevik-5a923515a/",
-  //     icon: "icons/social/linkedin.svg",
-  //     info: "My LinkedIn profile",
-  //   },
-  // ];
-
-  const visualIcons = [
-    {
-      type: "visual",
-      name: "Photoshop",
-      icon: "icons/software/photoshop.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "visual",
-      name: "After Effects",
-      icon: "icons/software/after-effects.png",
-      info: "Very good grasp of it's functions and capabilities",
-      level: 9,
-    },
-    {
-      type: "visual",
-      name: "Premiere Pro",
-      icon: "icons/software/premiere.png",
-      info: "Very good grasp of it's functions and capabilities",
-      level: 8,
-    },
-    {
-      type: "visual",
-      name: "DaVinci",
-      icon: "icons/software/davinci.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "visual",
-      name: "Blender",
-      icon: "icons/software/blender.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "visual",
-      name: "Nuke",
-      icon: "icons/software/nuke.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "visual",
-      name: "Unity",
-      icon: "icons/software/unity.png",
-      info: "Very good grasp of it's functions and capabilities.",
-      level: 8,
-    },
-    {
-      type: "visual",
-      name: "Unreal Engine",
-      icon: "icons/software/unreal.png",
-      info: "Very good grasp of it's functions and capabilities.",
-      level: 7,
-    },
-  ];
-
-  const audioIcons = [
-    {
-      type: "audio",
-      name: "Ableton",
-      icon: "icons/software/ableton.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "audio",
-      name: "Audacity",
-      icon: "icons/software/audacity.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "audio",
-      name: "Maschine",
-      icon: "icons/software/maschine.png",
-      info: "Excellent grasp of it's functions and capabilities",
-      level: 10,
-    },
-    {
-      type: "audio",
-      name: "Cubase",
-      icon: "icons/software/cubase.png",
-      info: "Very good grasp of it's functions and capabilities",
-      level: 7,
-    },
-  ];
-
-  const codeIcons = [
-    {
-      type: "code",
-      name: "HTML5",
-      icon: "icons/software/html.png",
-      info: "Excellent grasp of it's functions and capabilities.",
-      level: 10,
-    },
-    {
-      type: "code",
-      name: "CSS3",
-      icon: "icons/software/css.png",
-      info: "Excellent grasp of it's functions and capabilities.",
-      level: 10,
-    },
-    {
-      type: "code",
-      name: "JavaScript",
-      icon: "icons/software/javascript.png",
-      info: "Very good grasp of it's functions and capabilities.",
-      level: 8,
-    },
-    {
-      type: "code",
-      name: "Code",
-      icon: "icons/software/code.png",
-      info: "Very good grasp of it's functions and capabilities.",
-      level: 9,
-    },
-    {
-      type: "code",
-      name: "React",
-      icon: "icons/software/react.png",
-      info: "Very good grasp of it's functions and capabilities.",
-      level: 8,
-    },
-    {
-      type: "code",
-      name: "Git",
-      icon: "icons/software/git.png",
-      info: "Good grasp of it's functions and capabilities.",
-      level: 7,
-    },
-  ];
-
   const clickCopyHandler = (e) => {
-    // alert(e.target.textContent);
     navigator.clipboard.writeText(e.target.textContent);
   };
 
@@ -270,7 +107,7 @@ const About = (props) => {
               <p id="workplace">
                 FX3X STUDIO
                 <span>2018-2021</span>
-                <div id="circle"></div>
+                <span id="circle"></span>
               </p>
               <ul>
                 <p id="work-position">VFX Compositing Artist</p>
@@ -289,7 +126,7 @@ const About = (props) => {
               <p id="workplace">
                 FX3X STUDIO
                 <span>2015-2018</span>
-                <div id="circle"></div>
+                <span id="circle"></span>
               </p>
               <ul>
                 <span id="circle"></span>
@@ -305,19 +142,7 @@ const About = (props) => {
               </ul>
             </div>
           </div>
-          {/* <div className="about-section">
-            <div className="about-title">
-              <p>Interests</p>
-              <span></span>
-            </div>
-            <div className="right">
-              <ul>
-                <li>Web Developoment</li>
-                <li>3D & Graphic Design</li>
-                <li>Animation & Motion Graphics</li>
-              </ul>
-            </div>
-          </div> */}
+          
         </div>
       </div>
     </div>
